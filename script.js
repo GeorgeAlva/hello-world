0949ed59b430f67746f9267905445913156cf600
Jorge Alvarez Ramirez <jorgealva725@gmail.com>
2026-07-26 14:24:01 -0600

    add new class

--- script.js
+++ script.js
@@ -41,3 +41,21 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error(error);
     });
 });
+
+class ExampleThree {
+  static get properties() {
+    return {
+      /**
+       * name user
+       * @default ''
+       */
+      userName: { type: String },
+    };
+  }
+
+
+  constructor() {
+    super();
+    this.userName = 'juan';
+  }
+}
